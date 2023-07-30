721745d65fda1e5b44eabfbf36c4058adbf00018
Sahil <sahil2002ece@gmail.com>
2023-07-30 23:54:32 +0530

    Variables in js

---
+++ Variables/01_var.js
@@ -0,0 +1,8 @@
+//  There are four methods by which we can decleare variables
+ 
+ const accoundId = 1445 //value assigned to a variable cannot be changed
+ let accoundEmail = "Sahil@gmail.com" //value assigned to a variable can be changed
+ var passwords = 1234 //(Not preferable) value assigned to a variable can be changed 
+name= "sahil"
+ console.log(passwords);
+ console.log(name);
