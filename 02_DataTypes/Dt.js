e9a65f2e16a272070188c3e621a99d6cb207d6c4
Sahil <sahil2002ece@gmail.com>
2023-07-31 20:55:42 +0530

    Datatypes in js

---
+++ 02_DataTypes/Dt.js
@@ -0,0 +1,11 @@
+// variables in javascript
+let name="sahil"
+let rollno =20010104047
+let hostel = false
+let job = null
+let carrer = undefined
+let s = Symbol
+console.log(typeof s)
+console.log(typeof job);
+console.log(typeof carrer);
+console.log(hostel);
