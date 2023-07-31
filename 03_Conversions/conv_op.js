96faea3e52c0d8c688a5ce495cfccc5ee12a5353
Sahil <sahil2002ece@gmail.com>
2023-07-31 23:19:34 +0530

    DatType Coversions in js

---
+++ 03_Conversions/conv_op.js
@@ -0,0 +1,21 @@
+let score = "33"
+//Converting number To String
+let strScore=Number(score);
+console.log(strScore);
+console.log(typeof strScore);
+//Converting String to boolean
+let boolScore = Boolean(score)
+console.log(boolScore);
+console.log(typeof boolScore);
+//Special case
+let str = "33abc"
+let strToNum = Number(str)
+console.log(strToNum);
+
+//Converting num to string
+let num  = 123
+let numTostr = String(num)
+console.log(numTostr);
+//Converting num to boolean
+let numTobool = Boolean(num)
+console.log(numTobool);
